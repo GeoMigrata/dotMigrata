538c1b98e60e7c0a125403d7cf67095768d41db5
tsavpyn <tsavpyn@outlook.com>
2025-10-19 13:52:56 +0800

    Add utilities

---
+++ src/Common/Utilities/FactorValidator.cs
@@ -0,0 +1,13 @@
+﻿using dotGeoMigrata.Core.Domain.Entities;
+using dotGeoMigrata.Core.Domain.Values;
+
+namespace dotGeoMigrata.Common.Utilities;
+
+internal static class FactorValidator
+{
+    public static void EnsureDefined(World world, FactorDefinition factor)
+    {
+        if (!world.FactorDefinitions.Contains(factor))
+            throw new ArgumentException("Factor not defined", nameof(factor));
+    }
+}
