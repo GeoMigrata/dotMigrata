76e558440bcfa830466efca15674f7c4db20024c
tsavpyn <tsavpyn@outlook.com>
2025-10-19 13:53:29 +0800

    Add Coordinate.cs

---
+++ src/Core/Domain/Values/Coordinate.cs
@@ -0,0 +1,7 @@
+﻿namespace dotGeoMigrata.Core.Domain.Values;
+
+internal readonly struct Coordinate
+{
+    public required double Longitude { get; init; }
+    public required double Latitude { get; init; }
+}
